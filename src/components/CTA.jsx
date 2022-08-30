91caf07b198aee96c8e8cb17c0e0057ad61d325a
hussainHakimi <hussainbayan614@gmail.com>
2022-08-30 23:33:15 +0430

    client and CTA sections are done.

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
+import styles from "../styles"
+import Button from "./Button"
+
+const CTA = () => (
+  (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[5px] box-shadow justify-between`}>
+      <div className='flex flex-1 flex-col'>
+        <h2 className={`${styles.heading2}`}>Let’s try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div>
+      <Button styles='mt-10 md:mt-0'/>
+      </div>
+    </section>
   )
-}
+)
 
 export default CTA
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../styles'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () => (
+  (
+    <section className={`${styles.flexCenter} my-4 text-white`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`${styles.flexCenter} flex-1 sm:w-[192px] min-w-[120px] hover:cursor-pointer`}>
+            <img src={client.logo} alt="client"  className='w-[100px] sm:w-[192px] object-contain '/>
+          </div>
+        ))}
+      </div>
+    </section>
   )
-}
+)
 
 export default Clients
