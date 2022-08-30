f1550a9caf8db5c0c4b7f555320d181f034bf3eb
hussainHakimi <hussainbayan614@gmail.com>
2022-08-31 00:10:33 +0430

    second part of footer is alos done.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => (
           ))}
         </div>
       </div>
-      {/* <div className='flex justify-between w-full items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
+      <div className='flex justify-between w-full items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
         <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white mb-5'>2021 HooBank. All Rights Reserved.</p>
         <div className='flex flex-row md:mt-0 mt-6'>
           {socialMedia.map((social, index) => (
@@ -37,7 +37,7 @@ const Footer = () => (
               />
           ))}
         </div>
-      </div> */}
+      </div>
     </section>
   )
 )
