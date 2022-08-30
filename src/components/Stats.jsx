83651c9499d8cb82027e4f9ff13c05180fff3d44
hussainHakimi <hussainbayan614@gmail.com>
2022-08-30 21:04:46 +0430

    Business is done.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,6 +3,7 @@ import {stats} from '../constants'
 import styles from '../styles'
 
 
+
 const Stats = () => (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
       {stats.map((stat) =>(
