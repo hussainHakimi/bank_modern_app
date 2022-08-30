0fc38c793e9c1726e4d95a743bba4ce3f908aefb
hussainHakimi <hussainbayan614@gmail.com>
2022-08-30 15:19:22 +0430

    bank modern app is setted up.

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './styles'
+
+const App = () => (
+    <div className=' bg-primary overflow-hidden text-white w-full'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+    </div>
+  )
+
+
+export default App
