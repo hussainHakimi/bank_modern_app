9615f4cdeb2f0e3d5ccb9a0db6c384badb074d32
hussainHakimi <hussainbayan614@gmail.com>
2022-08-30 20:03:09 +0430

    stats is done.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => (
             The Next <br  className='sm:block hidden'/> {' '}
             <span className='text-gradient'>Generation</span> {" "}
           </h1>
-            <div className='ss:flex hidden md:mr-4 mr-0 text-gradient hover:bg-white'>
+            <div className='ss:flex hidden md:mr-4 mr-0 text-gradien'>
               <GetStarted/>
             </div>
         </div>
